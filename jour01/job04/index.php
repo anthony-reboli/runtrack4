a61b5c200d3c118822785f20a4c545959d1131dc
anthony.reboli <anthony.reboli@laplateforme.io>
2020-05-04 15:24:39 +0200

    job 4 fini

---
+++ jour01/job04/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<meta charset="UTF-8">
+	<title>run4job3</title>
+</head>
+<body>
+	<header>
+<nav role="navigation">
+  <div id="menuToggle">
+
+    <input type="checkbox" />
+    
+
+    <span></span>
+    <span></span>
+    <span></span>
+
+    <ul id="menu">
+      <a href="#"><li>accueil</li></a>
+      <a href="#"><li>Présentation</li></a>
+      <a href="#"><li>Contact</li></a>
+    </ul>
+  </div>
+</nav>
+	</header>
+	<main>
+
+		<section class="section">
+		</section>
+		<section class="section">
+		</section>
+		<section class="section">
+		</section>
+		<section class="section">
+		</section>
+	</main>
+
+	
+	
+</body>
+</html>
