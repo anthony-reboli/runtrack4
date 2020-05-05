43dccbc00ed66516a1e994f2f3d1e32a7bdb5280
anthony.reboli <anthony.reboli@laplateforme.io>
2020-05-05 13:43:52 +0200

    structure

---
+++ jour02/job01/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Job01</title>
+    <meta charset="UTF-8">
+  </head>
+<body>
+<hearder>
+  <nav>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Recherche</a></li>
+  </nav>
+</hearder>
+</body>
+<section>
+  <form action="">
+    <input type="radio">
+    <label>Mme</label>
+    <input type="radio">
+    <label>Mr</label>
+    <input type="text" placeholder="prenom">
+    <input type="text" placeholder="nom">
+    <input type="text" placeholder="adresse">
+    <input type="email" placeholder="email">
+    <input type="password" placeholder="password">
+    <input type="password" placeholder="confirm password">
+    <label>Passion(s) :</label>
+    <input type="checkbox">
+    <label>Informatique</label>
+    <input type="checkbox">
+    <label>Voyages</label>
+    <input type="checkbox">
+    <label>Sport</label>
+    <input type="checkbox">
+    <label>Lecture</label>
+    <input type="submit">
+  </form>
+</section>
+<footer>
+  <nav>
+    <li><a href="">Accueil</a></li>
+      <li><a href="">Inscription</a></li>
+      <li><a href="">Connexion</a></li>
+      <li><a href="">Recherche</a></li>
+  </nav>
+</footer>
+</html>
