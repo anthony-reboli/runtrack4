efd338c26bc92a18261c3a6fb9ad451ff48a448c
anthony.reboli <anthony.reboli@laplateforme.io>
2020-05-05 17:54:31 +0200

    fini

---
+++ jour02/job05/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Job05</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.7">
+		<!-- Compiled and minified CSS -->
+   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- ICONS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	</head>
+<body style="background-color: #87b7a4">
+<header>
+  <nav class="green">
+    <div>
+      <a href="" class="brand-logo"><img src="cam.png" width="50px">Seriestart</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="index.php">inscription</a></li>
+        <li><a href="index.php">connexion</a></li>
+      </ul>
+    </div>
+  </nav>
+</header><br><br>
+  <div class="row">
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+          <img src="naruto.png">
+          <span class="card-title">Naruto</span>
+        </div>
+        <div class="card-action">
+          <p>Un ados avec un démon ?!.</p>
+          <a href="https://fr.wikipedia.org/wiki/Naruto">Wikipedia</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="viking.png">
+          <span class="card-title">Viking</span>
+        </div>
+        <div class="card-action">
+          <p>Une histoire de viking. Ami ou Ennemi?</p>
+          <a href="https://fr.wikipedia.org/wiki/Vikings_(s%C3%A9rie_t%C3%A9l%C3%A9vis%C3%A9e)">Wikipedia</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="casa.png">
+          <span class="card-title">Casa de papel</span>
+        </div>
+        <div class="card-action">
+          <p>Un cerveau / du rouge ..</p>
+          <a href="https://fr.wikipedia.org/wiki/La_casa_de_papel">Wikipedia</a>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 m6 l3">
+      <div class="card">
+        <div class="card-image">
+           <img src="food.png">
+          <span class="card-title">Food Wars</span>
+        </div>
+        <div class="card-action">
+          <p>La classe social importe peut? Tous à vos poele!</p>
+          <a href="https://fr.wikipedia.org/wiki/Food_Wars!">Wikipedia</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</html>
