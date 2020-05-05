9375b1eb1bbe3730e7357804e8a12916061eb1a5
anthony.reboli <anthony.reboli@laplateforme.io>
2020-05-05 13:45:51 +0200

    footer

---
+++ jour02/job03/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Job03</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.7">
+    <!-- Compiled and minified CSS -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  </head>
+<body>
+<hearder>
+  <nav class="blue">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Recherche</a></li>
+      </ul>
+    </div>
+  </nav>
+</hearder>
+</body>
+<section>
+  <form action="">
+    <input type="radio">
+    <label>Mme</label>
+    <input type="radio">
+    <label>Mr</label>
+    <input type="text" placeholder="prenom">
+    <input type="text" placeholder="nom">
+    <input type="text" placeholder="adresse">
+    <input type="email" placeholder="email">
+    <input type="password" placeholder="password">
+    <input type="password" placeholder="confirm password">
+    <label>Passion(s) :</label>
+    <input type="checkbox">
+    <label>Informatique</label>
+    <input type="checkbox">
+    <label>Voyages</label>
+    <input type="checkbox">
+    <label>Sport</label>
+    <input type="checkbox">
+    <label>Lecture</label>
+    <input type="submit">
+  </form>
+</section>
+  <footer class="page-footer blue">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contenu</h5>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Lien</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="">Accueil</a></li>
+                  <li><a class="grey-text text-lighten-3" href="">Inscription</a></li>
+                  <li><a class="grey-text text-lighten-3" href="">Connexion</a></li>
+                  <li><a class="grey-text text-lighten-3" href="">Recherche</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
+</html>
