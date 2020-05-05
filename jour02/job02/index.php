90fe09dff79607895e5e4bf4abb21c6cd6463786
anthony.reboli <anthony.reboli@laplateforme.io>
2020-05-05 13:45:05 +0200

    header

---
+++ jour02/job02/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Job02</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.7">
+		<!-- Compiled and minified CSS -->
+   		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	</head>
+<body>
+<hearder>
+  <nav class="green">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">img</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Recherche</a></li>
+      </ul>
+    </div>
+  </nav>
+</hearder>
+</body>
+<section>
+	<form action="">
+		<input type="radio">
+		<label>Mme</label>
+		<input type="radio">
+		<label>Mr</label>
+		<input type="text" placeholder="prenom">
+		<input type="text" placeholder="nom">
+		<input type="text" placeholder="adresse">
+		<input type="email" placeholder="email">
+		<input type="password" placeholder="password">
+		<input type="password" placeholder="confirm password">
+		<label>Passion(s) :</label>
+		<input type="checkbox">
+		<label>Informatique</label>
+		<input type="checkbox">
+		<label>Voyages</label>
+		<input type="checkbox">
+		<label>Sport</label>
+		<input type="checkbox">
+		<label>Lecture</label>
+		<input type="submit">
+	</form>
+</section>
+<footer>
+	<nav class="green">
+		<li><a href="">Accueil</a></li>
+	    <li><a href="">Inscription</a></li>
+	    <li><a href="">Connexion</a></li>
+	    <li><a href="">Recherche</a></li>
+	</nav>
+</footer>
+</html>
